e662abfd922d0232faf5c6b7bdf0ccc38f1f2f79
Ryan <themolenator@gmail.com>
2018-03-13 20:14:20 -0400

    uploaded files

---
+++ flask_website/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1'
+            'body'  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Ryan Mills',
+            'created': '03-11-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2'
+            'body'  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Ryan Mills',
+            'created': '01-17-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3'
+            'body'  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Ryan Mills',
+            'created': '01-17-2018'
+        }
+    ]
+    return articles
